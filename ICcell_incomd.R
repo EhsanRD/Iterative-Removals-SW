4fab3a4f791e03edcf650384dec43dc0683ca1c4
EhsanRD <ehsan.rezaei.d@gmail.com>
2022-07-25 12:00:24 +1000

    Stop when variance of the treatment effect estimator is NA

--- ICcell_incomd.R
+++ ICcell_incomd.R
@@ -25,17 +25,17 @@ SWdesmat <- function(T) {
 # Xdlist
 # dlist
 # (K+1-k,T+1-t)
-m=100
-rho0=0.05
-r=0.95
+m=20
+rho0=0.01
+r=0.8
 #lbl="Hussey and Hughes model"
 #lbl="Exponential decay model"
 #1 decay 0 HH
 type=1
-T=5
+T=7
 # 0 removing without any consideration, 1 removing only one pair at each step
 cnum=1
-effsize=0.2
+effsize=0.32
 #
 # pal <- c("#FF4500", "#00FF00", "#00BFFF")
 # pal <- setNames(pal, c("0.95", "0.8","0.5"))
@@ -50,7 +50,7 @@ plot = function(data, m, T,rho0) {
   #relative variance plots accross cac
 plot_ly(data = res_m, x = ~na_percnt, y = ~Effloss, type="scatter",linetype=~as.factor(r),linetypes=linetypes,
         mode="lines", hoverinfo="text", hoverlabel=list(bordercolor=NULL, font=list(size=14)),
-        #,line=list(dash = "dash",color="#FF4500")
+        line=list(dash = "dash",color="#FF6666"),
         text=~paste("RPercent:", round(na_percnt,2),"%", "<br>Effloss:", round(Effloss,2),"%",
         "<br>Power:", format(round(power,4)*100,2),"%")) %>%
         layout(xaxis=list(title="Removal percentage of cluster-period cells (%)", titlefont=list(size=18), showline=TRUE,
@@ -69,23 +69,23 @@ plot_ly(data = res_m, x = ~na_percnt, y = ~Effloss, type="scatter",linetype=~as.
 }
 
 p=plot(res_m,m,T,rho0)
-save_image(p, file="effloss_paper1.png",width = 1150, height = 550)
+save_image(p, file="Eff_loss_plot.png",width = 1150, height = 550)
 
 ay <- list(
-  tickfont = list(color = "red"),
+  tickfont = list(color = "#00CC00"),
   overlaying = "y",
   side = "right",
   title = "<b>Power (%)</b>")
 
-
+###add power
 plot = function(data, m, T,rho0) {
   #relative variance plots accross cac
   plot_ly(data = res_m, x = ~na_percnt, y = ~Effloss, type="scatter",name = "Effloss", mode="lines",
          hoverinfo="text", hoverlabel=list(bordercolor=NULL, font=list(size=14)),line=list(dash ="dash",color="#FF6666"),
           text=~paste("RPercent:", round(na_percnt,2),"%", "<br>Effloss:", round(Effloss,2),"%",
                       "<br>Power:", round(power,2),"%")) %>%
-  add_trace(data = res_m, x = ~na_percnt, y = ~power, type="scatter",name = "power",mode="lines",
-            line=list(dash ="dot",color="#00CC00"), hoverinfo="text", hoverlabel=list(bordercolor=NULL, font=list(size=14))) %>% 
+  add_trace(data = res_m, x = ~na_percnt, y = ~power, type="scatter",name = "power",yaxis = "y2",mode="lines",
+            line=list(dash ="dot",color="#00CC00")) %>% 
   layout(yaxis2 = ay,xaxis=list(title="Removal percentage of cluster-period cells (%)", titlefont=list(size=18), showline=TRUE,
                                 tickmode="auto", tickfont=list(size=16), nticks=11, range=c(0,100),ticks="inside",
                                 mirror=TRUE, showgrid=FALSE),
@@ -103,6 +103,35 @@ plot = function(data, m, T,rho0) {
 p=plot(res_m,m,T,rho0)
 save_image(p, file="efflossPow_paper1.png",width = 1150, height = 550)
 
+###Paper
+####two separate icc (0.15, 0.14)
+plot = function(data, m, T,rho0) {
+plot_ly(data = res_m, x = ~na_percnt, y = ~Effloss, type="scatter",name = "Discrete-time Decay", mode="lines",
+        hoverinfo="text", hoverlabel=list(bordercolor=NULL, font=list(size=14)),line=list(dash ="dot",color="#FF6666"),
+        #,line=list(dash = "dash",color="#FF4500")
+        text=~paste("RPercent:", round(na_percnt,2),"%", "<br>Effloss:", round(Effloss,2),"%",
+                    "<br>Power:", round(power,2),"%")) %>%
+  layout(xaxis=list(title="Removal percentage of cluster-period cells (%)", titlefont=list(size=18), showline=TRUE,
+                    tickmode="auto", tickfont=list(size=16), nticks=11, range=c(0,100),ticks="inside",
+                    mirror=TRUE, showgrid=FALSE),
+         yaxis=list(title="Efficiency loss (%)", titlefont=list(size=18), tickfont=list(size=16),
+                    mirror=TRUE, showline=TRUE,range=c(0,100)),legend = list(title=list(text='<b>Type</b>')))%>% 
+  # title=list(text=paste0("\n","m=",m,","," ","T=",T,","," ","icc","=",rho0,","," ","effsize=",effsize),
+  #                      y =0.95,font=list(size = 15))) %>% 
+  add_segments(y = 0, yend = 50, x = 50, xend = 50,line = list(dash = "dash",color="#00BFFF"), showlegend=FALSE,hoverinfo='skip')%>%
+# title=list(text=paste(if (type == 1) {paste0("plot"," ",l," ","(","Discrete time decay",")")
+# } else if (type == 0){paste0("plot"," ",l," ","(","Exchangeable",")")},
+# paste0("\n","m=",m,","," ","T=",T,","," ","icc","=",rho0,","," ","effsize=",effsize))
+#                      ,y =0.95,font=list(size = 15)))
+  add_trace(data = res_m1, x = ~na_percnt, y = ~Effloss, type="scatter",name = "Exchangeable",mode="lines",
+            line=list(dash ="solid",color="#B266FF"))
+
+}
+
+
+p=plot(data,m,T,rho0)
+save_image(p, file="effloss_paper1_fig5.png",width = 1150, height = 550)
+
 l=0
 #cac = c(0.95,0.8,0.5,0.2)
 cac = 0.95
@@ -113,9 +142,9 @@ cac = 0.95
 # c(20,50,100)
 # c(0.01,0.05,0.1)
 # c(5,7,8,10)
-for (m in 50){
-  for (T in 10){
-    for (rho0 in 0.05){
+for (m in 90){
+  for (T in 5){
+    for (rho0 in 0.15){
       
       l=l+1
 
@@ -174,6 +203,7 @@ for (type in c(0,1)){
         
         #remove all low-infomration content cells and updating.
         for (i in 2:((T*K)/2)){
+        #lapply(2:((T*K)/2))
           mval[[i-1]] <- tryCatch(which(IC_func2(Xdlist[[i-1]],varmatall[i-1])==min(IC_func2(Xdlist[[i-1]],varmatall[i-1]),na.rm = TRUE), arr.ind = TRUE), warning=function(w) NA)
         
           if (is.na(mval[[i-1]][1])) {
@@ -207,8 +237,9 @@ for (type in c(0,1)){
                   }
            # }
           #varmatall[i] <- round(CRTVarGeneralAdj(Xdlist[[i]],m,rho0,r,type),10)
-          varmatall[i] <- tryCatch(CRTVarGeneralAdj(Xdlist[[i]],m,rho0,r,type),error=function(e) NA)
+          varmatall[i] <-tryCatch(CRTVarGeneralAdj(Xdlist[[i]],m,rho0,r,type),error=function(e) NA)
           if (is.na(varmatall[i])) {
+            Xdlist[[i]]<- NULL
             break
           }
           dlist[[i]] = IC_func2(Xdlist[[i]],varmatall[i])
@@ -234,7 +265,7 @@ for (type in c(0,1)){
          melted_desmatexcl<- melt(Xdlist)
          names(melted_desmatexcl)[names(melted_desmatexcl)=="value"] <- "Xdvalue"
          melted_varmatexcl_t<- merge(melted_varmatexcl, melted_desmatexcl,all.y=TRUE,by = c('Var1','Var2','L1'))
-         
+         melted_varmatexcl_t$value<-round(melted_varmatexcl_t$value, 4)
 
         #color_palette <-colorRampPalette(c( "yellow", "red"))(length(table(varmat_excl)))
 
@@ -296,28 +327,72 @@ for (type in c(0,1)){
     melted_varmatexcl_t <- merge(res, melted_varmatexcl_t, by = "iter", all = TRUE)
     
     
-    p<-ggplot(melted_varmatexcl_t,aes(x=Period, y=Sequence,fill=factor(value),frame=iter))+
-      geom_tile(colour = "grey50") +
+    p<-ggplot(melted_varmatexcl_t,aes(Period,Sequence,frame=iter))+
+      geom_tile(aes(fill=factor(value)),colour = "grey50") +
       scale_y_reverse(breaks=c(1:K)) +
       scale_x_continuous(breaks=c(1:T)) +
       theme(panel.grid.minor = element_blank()) +
       theme(legend.position="none")+
-      geom_label(data = melted_varmatexcl_t,aes(label= round(value,4)),fontface = "bold",
-                 colour = "white",size = 4) +
-      scale_fill_manual(values = pal, breaks=levels(melted_varmatexcl_t$value)[seq(90, 150, by=5)],
-                        na.value="gray")+
-      geom_text(data = melted_varmatexcl_t,aes(Period, Sequence,label= Xdvalue),
-              color = "black",size = 4,check_overlap = TRUE)
-
+      # geom_label(data = melted_varmatexcl_t,aes(label= round(value,4)),fontface = "bold",
+      #          colour = "white",size = 4) +
+      geom_text(aes(label= Xdvalue),color = "black",size = 7, check_overlap = T)+
+      #scale_fill_distiller
+      scale_fill_manual(values = pal,breaks=levels(melted_varmatexcl_t$value)[seq(90, 150, by=5)],na.value="gray")
+      
     p1<-ggplotly(p) %>% 
-      animation_opts(frame = 500,transition = 0,redraw = TRUE) %>%  
-      animation_slider(currentvalue = list(prefix = "Iter: ", font = list(color="orange"))) %>%
+      animation_opts(frame = 500,transition =0,redraw = TRUE) %>%  
+      animation_slider(currentvalue = list(prefix = "Iter: ", font = list(color="darkblue"))) %>%
       partial_bundle(local = FALSE)%>% 
-      animation_button(visible = F)%>%
-    animation_slider(visible = F)
+      animation_button(visible = T)%>%
+    animation_slider(visible =T)
+    #error seems ok: this is for geom_text
 p1
 
-# htmlwidgets::saveWidget(as_widget(p1), "index.html")
+htmlwidgets::saveWidget(as_widget(p1), "index.html")
+ 
+
+ 
+####PRESNTATION
+
+plot_list =list()
+add_name = "G:\\Shared drives\\Ehsan PhD work\\Ehsan\\Presentations\\ViCBiostat_July14\\schematic\\"
+ 
+for (i in unique(melted_varmatexcl_t$iter)) {
+  
+    png(paste0(add_name,i,".png"), width = 500, height = 500) 
+  
+    p<-ggplot(subset(melted_varmatexcl_t,iter==i),aes(Period,Sequence))+
+    geom_tile(aes(fill=factor(value)),colour = "grey50") +
+    scale_y_reverse(breaks=c(1:K)) +
+    scale_x_continuous(breaks=c(1:T)) +
+    theme(panel.grid.minor = element_blank()) +
+    theme(legend.position="none")+
+    # geom_label(data = melted_varmatexcl_t,aes(label= round(value,4)),fontface = "bold",
+    #          colour = "white",size = 4) +
+    geom_text(aes(label= value),color = "black",size = 4, check_overlap = T)+
+    #scale_fill_distiller
+    scale_fill_manual(values = pal,breaks=levels(factor(melted_varmatexcl_t$value)),na.value="gray") 
+  
+    plot_list[[i]] = p
+    print(plot_list[[i]])
+    dev.off()
+}
+
+#trial schemtaic
+color_palette <-colorRampPalette(c( "white", "grey"))(2)
+
+add_name = "G:\\Shared drives\\Ehsan PhD work\\Ehsan\\Presentations\\ViCBiostat_July14\\schematic\\"
+png(paste0(add_name,2,".png"), width = 500, height = 500) 
+ggplot(data =subset(melted_varmatexcl_t,iter==1), aes(x=Period, y=Sequence, fill = factor(Xdvalue))) + 
+  geom_tile( colour = "grey50") +
+  scale_y_reverse(breaks=c(1:K)) +
+  scale_x_continuous(breaks=c(1:T)) +
+  theme(panel.grid.minor = element_blank()) +           
+  geom_text(aes(Period, Sequence, label = Xdvalue), color = "black", size = 10) +
+  scale_fill_manual(values = color_palette) +  theme(legend.position="none")
+dev.off()
+
+
 # #relative variance plot
 # p <- plot_ly(res, height=500, width=800, x=~iter, y=~Rvariance, name="RVariance", type="scatter",
 #              mode="lines", hoverinfo="text", hoverlabel=list(bordercolor=NULL, font=list(size=16)),
