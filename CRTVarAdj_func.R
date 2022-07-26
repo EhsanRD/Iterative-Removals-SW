d775ee92bb21f6f93828a682da91eb2864bec5ea
EhsanRD <ehsan.rezaei.d@gmail.com>
2022-07-26 13:05:42 +1000

    Modify codes

--- CRTVarAdj_func.R
+++ CRTVarAdj_func.R
@@ -40,22 +40,23 @@ if (identical(M1,Xmat)==TRUE){
     stackI <- matrix(rep(diag(1,T)), nrow=K*T, ncol=T, byrow=TRUE)
     Zmat <- cbind(stackI[!is.na(Xvec),], Xvec[!is.na(Xvec)])
     
-    t=c()
-    t=which(colSums(is.na(Xmat)) == nrow(Xmat))
-    
+    tp=c()
+    tp=which(colSums(is.na(Xmat)) == nrow(Xmat))
+    fullT<-T+length(tp)
     #Variance matrix for one cluster, with decay in correlation over time
     #Vi <- diag(sig2,T) + matrix(sig2CP,nrow=T, ncol=T)
     #Constant decay var if type==0
     if(type==0) { 
-      Vi <-diag(sig2 +(1-r)*sig2CP, T+length(t)) + matrix(data=sig2CP*r, nrow=T+length(t), ncol=T+length(t))
+      Vi <-diag(sig2 +(1-r)*sig2CP, fullT) + matrix(data=sig2CP*r, nrow=fullT, ncol=fullT)
     }
     #exponential decay structure
-    if(type==1) { 
-      Vi <- diag(sig2,T+length(t)) + sig2CP*(r^abs(matrix(1:(T+length(t)),nrow=T+length(t), ncol=T+length(t), byrow=FALSE) - matrix(1:(T+length(t)),nrow=T+length(t), ncol=T+length(t), byrow=TRUE)))
+    if(type==1) {
+      Vi <- diag(sig2,fullT) + sig2CP*(r^abs(matrix(1:fullT,nrow=fullT, ncol=fullT, byrow=FALSE) - matrix(1:fullT,nrow=fullT, ncol=fullT, byrow=TRUE)))
       #Vi <- diag(sig2,T) + sig2CP*(r^abs(matrix(1:T,nrow=T, ncol=T, byrow=FALSE) - matrix(1:T,nrow=T, ncol=T, byrow=TRUE)))
     }
     #Variance matrix for all clusters
-    Vi <- Vi[-c(t[1:(length(t))]), -c(t[1:(length(t))])]
+    #Vi <- Vi[-c(t[1:(length(t))]), -c(t[1:(length(t))])]
+    Vi <- Vi[-tp,-tp]
 }
 
   #Variance matrix for all clusters
