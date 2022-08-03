bc7238d814208d5372be7e3e9ad990273b2fa140
EhsanRD <ehsan.rezaei.d@gmail.com>
2022-08-03 12:35:58 +1000

    Copy to the shiny app folder

---
+++ IterativeInfContent_v1/ICcell_appfunc.R
@@ -0,0 +1,155 @@
+#rm(list = ls())
+library("shiny")
+library("ggplot2")
+library("reshape2")
+library("plyr")
+library("swCRTdesign")
+library("matrixcalc")
+library("scales")
+library("tidyverse")
+library("shinythemes")
+library("Matrix")
+library("plotly")
+library("RColorBrewer")
+library("gganimate")
+library("gifski")
+library("cargo")
+library("tidyr")
+library("animation")
+library("htmlwidgets")
+library("plotly")
+library("manipulateWidget")
+library("htmlwidgets")
+
+# Functions for generating design matrices
+SWdesmat <- function(T) {
+  Xsw <- matrix(data=0, ncol = T, nrow = (T-1))
+  for(i in 1:(T-1)) {
+    Xsw[i,(i+1):T] <- 1
+  }
+  return(Xsw)
+}
+
+###SWdesmat with implementation periods
+SWdesmatIMP <- function(T, nIMP) {
+  Xsw <- SWdesmat(T+nIMP)
+  for(i in 1:T) Xsw[i, (i+1):(i+nIMP)] <- NA
+  
+  return(Xsw[1:(T-1),])
+}
+
+#Designs missing cluster-period cells:
+
+###this calculates the variance of the treatment effect estimator for the given inputs:###
+
+#General function (taken from ExpDecayVar.r in Information content folder)
+CRTVarGeneral <- function(Xmat, m, rho0, r, type) {
+
+  totalvar <- 1
+  sig2CP <- rho0*totalvar
+  sig2E <- totalvar - sig2CP
+
+  sig2 <- sig2E/m
+
+  T <- ncol(Xmat)
+  K <- nrow(Xmat)
+
+  
+  Xvec <- as.vector(t(Xmat))
+  stackI <- matrix(rep(diag(1,T)), nrow=K*T, ncol=T, byrow=TRUE)
+  Zmat <- cbind(stackI[!is.na(Xvec),], Xvec[!is.na(Xvec)])
+
+  #Variance matrix for one cluster, with decay in correlation over time
+  #Vi <- diag(sig2,T) + matrix(sig2CP,nrow=T, ncol=T)
+  #Constant decay var if type==0
+  if(type==0) { 
+    Vi <-diag(sig2 +(1-r)*sig2CP, T) + matrix(data=sig2CP*r, nrow=T, ncol=T)
+  }
+  #exponential decay structure
+  if(type==1) { 
+    Vi <- diag(sig2,T) + sig2CP*(r^abs(matrix(1:T,nrow=T, ncol=T, byrow=FALSE) - matrix(1:T,nrow=T, ncol=T, byrow=TRUE)))
+  }
+  #Variance matrix for all clusters
+  Vall <- kronecker(diag(1,K), Vi)
+  Vall <- Vall[!is.na(Xvec),!is.na(Xvec)]
+
+  #vartheta <- solve((t(Zmat)%*%solve(Vall)%*%Zmat))[ncol(Zmat),ncol(Zmat)]
+
+  #xtry <- try(solve((t(Zmat)%*%solve(Vall)%*%Zmat))) 
+  #if('try-error' %in% class(xtry)) return(NA)
+
+  #there will be problems if Zmat is not of full column rank
+  if(rankMatrix(Zmat)[1] < ncol(Zmat)) return(NA)
+  else return(solve((t(Zmat)%*%solve(Vall)%*%Zmat))[ncol(Zmat),ncol(Zmat)])
+
+  #return(vartheta)
+}
+
+#Designs missing an entire period incorporating discrete-time decay
+
+CRTVarGeneralT <- function(Xmat, ocol, m, rho0, r, type) {
+  
+  totalvar <- 1
+  sig2CP <- rho0*totalvar
+  sig2E <- totalvar - sig2CP
+  
+  sig2 <- sig2E/m
+  
+  T <- ncol(Xmat[,-ocol])
+  K <- nrow(Xmat)
+  Xvec <- as.vector(t(Xmat[,-ocol]))
+  
+  stackI <- matrix(rep(diag(1,T)), nrow=K*T, ncol=T, byrow=TRUE)
+  Zmat <- cbind(stackI[!is.na(Xvec),], Xvec[!is.na(Xvec)])
+  
+  #Variance matrix for one cluster, with decay in correlation over time
+  #Vi <- diag(sig2,T) + matrix(sig2CP,nrow=T, ncol=T)
+  #Constant decay var if type==0
+  if(type==0) { 
+    Vi <-diag(sig2 +(1-r)*sig2CP, T+1) + matrix(data=sig2CP*r, nrow=T+1, ncol=T+1)
+  }
+  #exponential decay structure
+  if(type==1) { 
+    Vi <- diag(sig2,T+1) + sig2CP*(r^abs(matrix(1:(T+1),nrow=T+1, ncol=T+1, byrow=FALSE) - matrix(1:(T+1),nrow=T+1, ncol=T+1, byrow=TRUE)))
+    #Vi <- diag(sig2,T) + sig2CP*(r^abs(matrix(1:T,nrow=T, ncol=T, byrow=FALSE) - matrix(1:T,nrow=T, ncol=T, byrow=TRUE)))
+  }
+  #Variance matrix for all clusters
+  Vi <- Vi[-ocol, -ocol]
+  
+  #Variance matrix for all clusters
+  Vall <- kronecker(diag(1,K), Vi)
+  Vall <- Vall[!is.na(Xvec),!is.na(Xvec)]
+  
+  #Variance of the treatment effect estimator is then given by:
+  return(solve((t(Zmat)%*%solve(Vall)%*%Zmat))[ncol(Zmat),ncol(Zmat)] )
+  
+  #return(vartheta)
+}
+
+#Generate variance results for user-defined trial configuration, with progress bar
+# generate_var_results_prog <- function(m, rho0, type,r, updateProgress = NULL) {
+#   
+#   T <- seq(2,20,1)
+#   
+#   if (is.function(updateProgress)) {
+#     updateProgress()
+#   }
+#   
+#   SWXmat <-lapply(T,SWdesmat(T))
+#   Xmats <-SWXmat
+#   if (is.function(updateProgress)) {
+#     updateProgress()
+#   }
+# 
+#   ctres <- llply(T,CRTVarGeneral, m=m, rho0=rho0,type=type,r=r, Xmat=Xmats)
+# 
+#   if (is.function(updateProgress)) {
+#     updateProgress()
+#   }
+#   varvals <- ctres[,1]
+#   return(varvals)
+# }
+# generate_var_results_prog(m=100,rho0=0.05,type=1,r=.8)
+
+  
+  
