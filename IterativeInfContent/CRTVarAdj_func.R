8a8a9f8b999d58dfed4e1f5ee3127a4cf79aaf6d
EhsanRD <ehsan.rezaei.d@gmail.com>
2022-10-21 15:01:51 +1100

    Streamline the function & no longer use the reduced matrix

--- IterativeInfContent/CRTVarAdj_func.R
+++ IterativeInfContent/CRTVarAdj_func.R
@@ -13,8 +13,7 @@ CRTVarGeneralAdj <- function(Xmat, m, rho0, r, type) {
   sig2E <- totalvar - sig2CP
   
   sig2 <- sig2E/m
-  M1=Xmat[,colSums(!is.na(Xmat)) > 0]
-  
+  ##M1=Xmat[,colSums(!is.na(Xmat)) > 0]
 # if (identical(M1,Xmat)==TRUE){
 #     
 #     T <- ncol(Xmat)
@@ -41,32 +40,39 @@ CRTVarGeneralAdj <- function(Xmat, m, rho0, r, type) {
 #   
 #     else {
 
-    T <- ncol(M1)
-    K <- nrow(Xmat)
-    Xvec <- as.vector(t(M1))
+    Tp <- ncol(Xmat)
+    K  <- nrow(Xmat)
+    Xvec <- as.vector(t(Xmat))
     
-    stackI <- matrix(rep(diag(1,T)), nrow=K*T, ncol=T, byrow=TRUE)
-    Zmat <- cbind(stackI[!is.na(Xvec),], Xvec[!is.na(Xvec)])
+    stackI <- matrix(rep(diag(1,Tp)), nrow=K*Tp, ncol=Tp, byrow=TRUE)
+    ##Zmat <- cbind(stackI[!is.na(Xvec),], Xvec[!is.na(Xvec)])
+    Zmat <- cbind(stackI, Xvec)
+    Zmat <- Zmat[!is.na(Xvec),]
     
-    tp=c()
-    tp=which(colSums(is.na(Xmat)) == nrow(Xmat))
-    fullT<-T+length(tp)
+    dropind=which(colSums(!is.na(Xmat)) == 0)
+    
+    if(length(dropind>0)){
+      Zmat <- Zmat[,-dropind]
+    }
+    
+    ##fullT<-T+length(dropind)
     #Variance matrix for one cluster, with decay in correlation over time
     #Vi <- diag(sig2,T) + matrix(sig2CP,nrow=T, ncol=T)
     #Constant decay var if type==0
     if(type==0) { 
-      Vi <-diag(sig2 +(1-r)*sig2CP, fullT) + matrix(data=sig2CP*r, nrow=fullT, ncol=fullT)
+      Vi <-diag(sig2 +(1-r)*sig2CP, Tp) + matrix(data=sig2CP*r, nrow=Tp, ncol=Tp)
     }
     #exponential decay structure
     if(type==1) {
-      Vi <- diag(sig2,fullT) + sig2CP*(r^abs(matrix(1:fullT,nrow=fullT, ncol=fullT, byrow=FALSE) - matrix(1:fullT,nrow=fullT, ncol=fullT, byrow=TRUE)))
+      Vi <- diag(sig2,Tp) + sig2CP*(r^abs(matrix(1:Tp,nrow=Tp, ncol=Tp, byrow=FALSE) -
+                          matrix(1:Tp,nrow=Tp, ncol=Tp, byrow=TRUE)))
       #Vi <- diag(sig2,T) + sig2CP*(r^abs(matrix(1:T,nrow=T, ncol=T, byrow=FALSE) - matrix(1:T,nrow=T, ncol=T, byrow=TRUE)))
     }
     #Variance matrix for all clusters
     #Vi <- Vi[-c(t[1:(length(t))]), -c(t[1:(length(t))])]
-    if (identical(M1,Xmat)==FALSE){
-    Vi <- Vi[-tp,-tp]
-    }
+    ##  if (identical(M1,Xmat)==FALSE){
+    ## Vi <- Vi[-tp,-tp]
+    ## }
 #}
 
   #Variance matrix for all clusters
