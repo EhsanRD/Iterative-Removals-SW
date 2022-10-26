bd7cfe4de6d7af568337213e202ba3c2652f490b
EhsanRD <ehsan.rezaei.d@gmail.com>
2022-10-26 13:49:20 +1100

    Streamline the codes

--- IterativeInfContent/CRTVarAdj_func.R
+++ IterativeInfContent/CRTVarAdj_func.R
@@ -1,8 +1,8 @@
 # Functions for generating design matrices
-SWdesmat <- function(T) {
-  Xsw <- matrix(data=0, ncol = T, nrow = (T-1))
-  for(i in 1:(T-1)) {
-    Xsw[i,(i+1):T] <- 1
+SWdesmat <- function(Tp) {
+  Xsw <- matrix(data=0, ncol = Tp, nrow = (Tp-1))
+  for(i in 1:(Tp-1)) {
+    Xsw[i,(i+1):Tp] <- 1
   }
   return(Xsw)
 }
@@ -11,41 +11,13 @@ CRTVarGeneralAdj <- function(Xmat, m, rho0, r, type) {
   totalvar <- 1
   sig2CP <- rho0*totalvar
   sig2E <- totalvar - sig2CP
-  
   sig2 <- sig2E/m
-  ##M1=Xmat[,colSums(!is.na(Xmat)) > 0]
-# if (identical(M1,Xmat)==TRUE){
-#     
-#     T <- ncol(Xmat)
-#     K <- nrow(Xmat)
-#     
-#     Xvec <- as.vector(t(Xmat))
-#     stackI <- matrix(rep(diag(1,T)), nrow=K*T, ncol=T, byrow=TRUE)
-#     Zmat <- cbind(stackI[!is.na(Xvec),], Xvec[!is.na(Xvec)])
-#     
-#     #Variance matrix for one cluster, with decay in correlation over time
-#     #Vi <- diag(sig2,T) + matrix(sig2CP,nrow=T, ncol=T)
-#     #Constant decay var if type==0
-#     if(type==0) {
-#       Vi <-diag(sig2 +(1-r)*sig2CP, T) + matrix(data=sig2CP*r, nrow=T, ncol=T)
-#     }
-#     #exponential decay structure
-#     if(type==1) {
-#       Vi <- diag(sig2,T) + sig2CP*(r^abs(matrix(1:T,nrow=T, ncol=T, byrow=FALSE) - matrix(1:T,nrow=T, ncol=T, byrow=TRUE)))
-#     }
-# }  
-#   #Need to create the Vi for the *original* design matrix
-#   #and then chop out the relevant row and column
-#   #T+length(t) is to allow for this.
-#   
-#     else {
-
+  
     Tp <- ncol(Xmat)
     K  <- nrow(Xmat)
     Xvec <- as.vector(t(Xmat))
     
     stackI <- matrix(rep(diag(1,Tp)), nrow=K*Tp, ncol=Tp, byrow=TRUE)
-    ##Zmat <- cbind(stackI[!is.na(Xvec),], Xvec[!is.na(Xvec)])
     Zmat <- cbind(stackI, Xvec)
     Zmat <- Zmat[!is.na(Xvec),]
     
@@ -55,7 +27,6 @@ CRTVarGeneralAdj <- function(Xmat, m, rho0, r, type) {
       Zmat <- Zmat[,-dropind]
     }
     
-    ##fullT<-T+length(dropind)
     #Variance matrix for one cluster, with decay in correlation over time
     #Vi <- diag(sig2,T) + matrix(sig2CP,nrow=T, ncol=T)
     #Constant decay var if type==0
@@ -66,28 +37,14 @@ CRTVarGeneralAdj <- function(Xmat, m, rho0, r, type) {
     if(type==1) {
       Vi <- diag(sig2,Tp) + sig2CP*(r^abs(matrix(1:Tp,nrow=Tp, ncol=Tp, byrow=FALSE) -
                           matrix(1:Tp,nrow=Tp, ncol=Tp, byrow=TRUE)))
-      #Vi <- diag(sig2,T) + sig2CP*(r^abs(matrix(1:T,nrow=T, ncol=T, byrow=FALSE) - matrix(1:T,nrow=T, ncol=T, byrow=TRUE)))
     }
-    #Variance matrix for all clusters
-    #Vi <- Vi[-c(t[1:(length(t))]), -c(t[1:(length(t))])]
-    ##  if (identical(M1,Xmat)==FALSE){
-    ## Vi <- Vi[-tp,-tp]
-    ## }
-#}
 
   #Variance matrix for all clusters
   Vall <- kronecker(diag(1,K), Vi)
   Vall <- Vall[!is.na(Xvec),!is.na(Xvec)]
   
-  #vartheta <- solve((t(Zmat)%*%solve(Vall)%*%Zmat))[ncol(Zmat),ncol(Zmat)]
-  
-  #xtry <- try(solve((t(Zmat)%*%solve(Vall)%*%Zmat)))
-  #if('try-error' %in% class(xtry)) return(NA)
-  
   #there will be problems if Zmat is not of full column rank
   if(rankMatrix(Zmat)[1] < ncol(Zmat)) return(NA)
   else return(solve((t(Zmat)%*%solve(Vall)%*%Zmat))[ncol(Zmat),ncol(Zmat)])
-  
-  #return(vartheta)
 }
 
