1f6395723dedf8c0a230376dc3b57d6b0e9c9218
EhsanRD <ehsan.rezaei.d@gmail.com>
2022-12-13 16:46:53 +1100

    make efficient and tidy

--- IterativeInfContent/IterRemoval_func.R
+++ IterativeInfContent/IterRemoval_func.R
@@ -26,10 +26,11 @@ library("rsconnect")
 require("gridExtra")
 
 #setwd("~/Google Drive/Shared drives/Ehsan PhD work/Codes/")
-setwd("G:\\Shared drives\\Ehsan PhD work\\Codes\\Git\\Iterative-Removals-SW\\IterativeInfContent")
+#setwd("G:\\Shared drives\\Ehsan PhD work\\Codes\\Git\\Iterative-Removals-SW\\IterativeInfContent")
 source("CRTVarAdj_func.R", local=TRUE)
 #source("ICcell_appfunc.R", local=TRUE)
-# Functions for generating design matrices
+
+#function for generating design matrices
 SWdesmat <- function(Tp) {
   Xsw <- matrix(data=0, ncol = Tp, nrow = (Tp-1))
   for(i in 1:(Tp-1)) {
@@ -38,73 +39,78 @@ SWdesmat <- function(Tp) {
   return(Xsw)
 }
 #######
-IterRemoval <- function(Tp,m, rho0, r, type,cpnum) {
 
-        K=Tp-1
-        Xdes <- SWdesmat(Tp)
-        varmatall <- c()
-        varmatall<- CRTVarGeneralAdj(Xdes,m,rho0,r,type)
-        varmat_excl<-matrix(data=NA, nrow=nrow(Xdes), ncol=ncol(Xdes))
-        
-        
-        #Need to change the way of coding
-        IC_func2 = function(Xdes,varmatall){
-          for(i in 1:nrow(Xdes)){
-            for (j in 1:ncol(Xdes)){
-              if(is.na(Xdes[i,j])==TRUE | is.na(Xdes[K-i+1,Tp-j+1])==TRUE){
-                varmat_excl[i,j] <- NA
-                varmat_excl[K-i+1,Tp-j+1] <- NA
-              }
-              else if(is.na(Xdes[i,j])==FALSE & is.na(Xdes[K-i+1,Tp-j+1])==FALSE) {
-                Xdesij <- Xdes
-                Xdesij[i,j] <- NA
-                Xdesij[K-i+1,Tp-j+1] <- NA
-                varmat_excl[i,j] <- CRTVarGeneralAdj(Xdesij,m,rho0,r,type)/varmatall
-                varmat_excl[K-i+1,Tp-j+1] <- varmat_excl[i,j]
-              }
-            }
-          }
-          return(varmat_excl)
-        }
-        mval <- list()
-        Xdlist <- list()
-        dlist <- list()
-        Xdlist[[1]] <- Xdes
-        dlist [[1]]<- IC_func2(Xdlist[[1]],varmatall)
-        varmatall[1] <- varmatall
-        
-        #remove all low-infomration content cells and updating.
-        for (i in 2:((Tp*K)/1)){
-        #lapply(2:((T*K)/2))
-          mval[[i-1]] <- tryCatch(which(IC_func2(Xdlist[[i-1]],varmatall[i-1])==min(IC_func2(Xdlist[[i-1]],varmatall[i-1]),na.rm = TRUE), arr.ind = TRUE), warning=function(w) NA)
-        
-          if (is.na(mval[[i-1]][1])) {
-            dlist[[i-1]]<- NULL
-            break
-          }
-          
-          Xdlist[[i]]=Xdlist[[i-1]]
-          
-            if (cpnum==0){
-          # Loop is not good here, chenge it in future
-          tryCatch(for (j in 1:dim(mval[[i-1]])[1]){
-            Xdlist[[i]][mval[[i-1]][[j]],mval[[i-1]][[dim(mval[[i-1]])[1]+j]]]<- NA
-            Xdlist[[i]][K+1-mval[[i-1]][[j]],Tp+1-mval[[i-1]][[dim(mval[[i-1]])[1]+j]]]<- NA
-          }, error=function(e) NA)
-            }
-          #remove the smallest cluster and period, and removing the corresponding pair. 
-          else if (cpnum==1){
-                    mval[[i-1]] <- mval[[i-1]][order(mval[[i-1]][,1],mval[[i-1]][,2]),]
-                    Xdlist[[i]][mval[[i-1]][[1]],mval[[i-1]][[dim(mval[[i-1]])[1]+1]]]<- NA
-                    Xdlist[[i]][K+1-mval[[i-1]][[1]],Tp+1-mval[[i-1]][[dim(mval[[i-1]])[1]+1]]]<- NA
-                  }
-          varmatall[i] <-tryCatch(CRTVarGeneralAdj(Xdlist[[i]],m,rho0,r,type),error=function(e) NA)
-          if (is.na(varmatall[i])) {
-            Xdlist[[i]]<- NULL
-            break
-          }
-          dlist[[i]] = IC_func2(Xdlist[[i]],varmatall[i])
-        }
-        return(list(dlist,Xdlist,varmatall))
+#assume one cluster is randomised to each sequence   
 
+#function to calculate the information contents utilising centrosymmetric property 
+ICPair = function(Xdes,m,rho0,r,type){
+  
+  Tp <- ncol(Xdes)
+  K  <- nrow(Xdes)
+  varD=CRTVarGeneralAdj(Xdes,m,rho0,r,type)
+  ICmat<-matrix(data=NA, nrow=nrow(Xdes), ncol=ncol(Xdes))
+  
+    for(i in 1:nrow(Xdes)){
+      for (j in 1:ncol(Xdes)){
+        if(is.na(Xdes[i,j])==TRUE | is.na(Xdes[K-i+1,Tp-j+1])==TRUE){
+          ICmat[i,j] <- NA
+          ICmat[K-i+1,Tp-j+1] <- NA
+        }
+        else if(is.na(Xdes[i,j])==FALSE & is.na(Xdes[K-i+1,Tp-j+1])==FALSE) {
+          Xdesij <- Xdes
+          Xdesij[i,j] <- NA
+          Xdesij[K-i+1,Tp-j+1] <- NA
+          ICmat[i,j] <- CRTVarGeneralAdj(Xdesij,m,rho0,r,type)/varD
+          ICmat[K-i+1,Tp-j+1] <- ICmat[i,j]
+        }
+        #avoid replicating loop
+        if (i*j==(nrow(Xdes)*ncol(Xdes)/2)){
+          break
+        }
+      }
+    }
+  return(ICmat)
+}
+
+
+IterRemove = function(Tp,m,rho0,r,type){
+  K=Tp-1
+  mval <- list()    #minimum lowest information content values
+  Xdlist <- list()  #design matrix
+  dlist <- list()   #information content matrix
+  Xdlist[[1]] <- SWdesmat(Tp)
+  dlist[[1]] <- ICPair(Xdlist[[1]],m,rho0,r,type)
+  
+  varvec<- c()
+  varvec[1]<-CRTVarGeneralAdj(Xdlist[[1]],m,rho0,r,type)
+  #removal of pairs of cells
+  for (i in 2:(Tp*K/2-1)){ #most minimal design 
+    mval <- which(dlist[[i-1]]==min(dlist[[i-1]],na.rm = TRUE), arr.ind = TRUE)
+    
+    # if (is.na(mval[1])) {
+    #   dlist[[i-1]]<- NULL
+    #   break
+    #    }
+    
+    Xdlist[[i]]=Xdlist[[i-1]]
+    #remove a pair of centrosymmetric cells
+    #re-order indices by cluster and period
+    mval <- mval[order(mval[,1],mval[,2]),]
+    cellid <- mval[1,]
+    clustid<-cellid[1]
+    perid<-cellid[2]
+    Xdlist[[i]][clustid,perid]<- NA
+    Xdlist[[i]][K+1-clustid,Tp+1-perid]<- NA
+    
+    varvec[i]<-CRTVarGeneralAdj(Xdlist[[i]],m,rho0,r,type)#modify CRTVarGeneralAdj to give Na if variance cannt be calculated
+    
+    if (is.na(varvec[i])) {
+    Xdlist[[i]]<- NULL
+    break
+    }
+    
+    dlist[[i]] = ICPair(Xdlist[[i]],m,rho0,r,type)
+    #checking if dlist contains all NAs, if yes calculate the variance one more time
+   }
+  return(list(dlist,Xdlist, varvec))
 }
