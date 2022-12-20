cc6d8dd1c0a14b6e09a6fe5d61efc0d789059c09
EhsanRD <ehsan.rezaei.d@gmail.com>
2022-12-20 16:04:58 +1100

    Be ready for submission

--- IterativeInfContent/IterRemoval_func.R
+++ IterativeInfContent/IterRemoval_func.R
@@ -1,35 +1,4 @@
-# install.packages("rsconnect")
-# library("rsconnect")
-
-library("shiny")
-library("ggplot2")
-library("reshape2")
-library("plyr")
-library("swCRTdesign")
-library("matrixcalc")
-library("scales")
-library("tidyverse")
-library("shinythemes")
-library("Matrix")
-library("plotly")
-library("RColorBrewer")
-library("gganimate")
-library("gifski")
-#library("cargo")
-library("tidyr")
-library("animation")
-library("htmlwidgets")
-library("plotly")
-#library("manipulateWidget")
-#library("htmlwidgets")
-library("rsconnect")
-require("gridExtra")
-
-#setwd("~/Google Drive/Shared drives/Ehsan PhD work/Codes/")
-#setwd("G:\\Shared drives\\Ehsan PhD work\\Codes\\Git\\Iterative-Removals-SW\\IterativeInfContent")
 source("CRTVarAdj_func.R", local=TRUE)
-#source("ICcell_appfunc.R", local=TRUE)
-
 #function for generating design matrices
 SWdesmat <- function(Tp) {
   Xsw <- matrix(data=0, ncol = Tp, nrow = (Tp-1))
@@ -39,9 +8,7 @@ SWdesmat <- function(Tp) {
   return(Xsw)
 }
 #######
-
 #assume one cluster is randomised to each sequence   
-
 #function to calculate the information contents utilising centrosymmetric property 
 ICPair = function(Xdes,m,rho0,r,type){
   
@@ -91,12 +58,6 @@ IterRemove = function(Tp,m,rho0,r,type){
   #removal of pairs of cells
   for (i in 2:(Tp*K/2-1)){ #most minimal design 
     mval <- which(dlist[[i-1]]==min(dlist[[i-1]],na.rm = TRUE), arr.ind = TRUE)
-    
-    # if (is.na(mval[1])) {
-    #   dlist[[i-1]]<- NULL
-    #   break
-    #    }
-    
     Xdlist[[i]]=Xdlist[[i-1]]
     #remove a pair of centrosymmetric cells
     #re-order indices by cluster and period
@@ -107,15 +68,10 @@ IterRemove = function(Tp,m,rho0,r,type){
     Xdlist[[i]][clustid,perid]<- NA
     Xdlist[[i]][K+1-clustid,Tp+1-perid]<- NA
     
-    varvec[i]<-CRTVarGeneralAdj(Xdlist[[i]],m,rho0,r,type)#modify CRTVarGeneralAdj to give Na if variance cannt be calculated
+    varvec[i]<-CRTVarGeneralAdj(Xdlist[[i]],m,rho0,r,type)
     
-    # if (is.na(varvec[i])) {
-    # Xdlist[[i]]<- NULL
-    # break
-    # }
-    # 
     dlist[[i]] = ICPair(Xdlist[[i]],m,rho0,r,type)
-    #checking if dlist contains all NAs, if yes calculate the variance one more time
+
    }
   return(list(dlist,Xdlist, varvec))
 }
